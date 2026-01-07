0cb00c9c60701dc4ec6745a975718d4e6e4e59d0
Enzo1401 <154237934+Enzo1401@users.noreply.github.com>
2026-01-07 10:53:31 +0100

    Add files via upload

---
+++ list_and_dict.py
@@ -0,0 +1,14 @@
+# Copiez ce fichier dans votre repo PERSONNEL
+# Tapez votre code ci dessous
+# puis executer ce fichier dans un terminal avec la commande "py ex_basic1_1.py"
+
+# Init variables
+teams_points = {
+    "Paris SG": 76,
+    "Monaco": 67,
+    "Brest": 61,
+    "Lille": 59,
+    "Nice": 55,
+    "Lyon": 53,
+    "Lens": 51,
+}
